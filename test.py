593808a67765d795fa43783efea1349ed0ca25b9
Kalle Kromann <kalle.kromann@laerdal.com>
2023-10-27 13:56:03 +0200

    Variable paylaod

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import requests
-import json
+import sys
+
+payload = sys.argv[1]
+#payload = json.dumps({"on": True})
 
 url = 'https://api.meethue.com/bridge/KvZfKvjp4mefjSVg2J1WJv7nQZQ7jfDaaXlFfJ2e/lights/1/state'
-payload = json.dumps({"on": True})
+
 headers = {'content-type': 'application/json', 'Authorization': 'Bearer E8rNJawB8g96BqrT5SAgG3tIPOP6'}
 r = requests.post(url, data=payload, headers=headers)
