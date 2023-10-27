6a338c815710175b59f43f49a86a42f2bc0d243a
Kalle Kromann <kalle.kromann@laerdal.com>
2023-10-27 13:18:43 +0200

    Test turn off

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
+
 curl --location --request PUT 'https://api.meethue.com/bridge/KvZfKvjp4mefjSVg2J1WJv7nQZQ7jfDaaXlFfJ2e/lights/1/state' \
      --header 'Content-Type: application/json' \
      --header 'Authorization: Bearer E8rNJawB8g96BqrT5SAgG3tIPOP6' \
-     --data '{"on":true}'
+     --data '{"on":false}'
