8ff39675886c036e82c0d3af9d062a287826b273
Kalle Kromann <kalle.kromann@laerdal.com>
2023-10-27 13:20:36 +0200

    Off again

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 
-curl --location --request PUT 'https://api.meethue.com/bridge/KvZfKvjp4mefjSVg2J1WJv7nQZQ7jfDaaXlFfJ2e/lights/1/state' \
+curl -k --location --request PUT 'https://api.meethue.com/bridge/KvZfKvjp4mefjSVg2J1WJv7nQZQ7jfDaaXlFfJ2e/lights/1/state' \
      --header 'Content-Type: application/json' \
      --header 'Authorization: Bearer E8rNJawB8g96BqrT5SAgG3tIPOP6' \
      --data '{"on":false}'
