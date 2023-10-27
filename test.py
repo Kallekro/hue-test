090de25ff0611896550af1434100061d66250ffa
Kalle Kromann <kalle.kromann@laerdal.com>
2023-10-27 14:02:33 +0200

    Put

--- test.py
+++ test.py
@@ -8,5 +8,5 @@ print(payload)
 url = 'https://api.meethue.com/bridge/KvZfKvjp4mefjSVg2J1WJv7nQZQ7jfDaaXlFfJ2e/lights/1/state'
 
 headers = {'content-type': 'application/json', 'Authorization': 'Bearer E8rNJawB8g96BqrT5SAgG3tIPOP6'}
-r = requests.post(url, data=payload, headers=headers)
+r = requests.put(url, data=payload, headers=headers)
 print(r.text)
