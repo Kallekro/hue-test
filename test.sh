a11a79d539f7643dda6810d5c29268563b303e52
Kalle Kromann <kalle.kromann@laerdal.com>
2023-10-27 13:24:18 +0200

    On

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
+ON=$1
 
 curl -k --location --request PUT 'https://api.meethue.com/bridge/KvZfKvjp4mefjSVg2J1WJv7nQZQ7jfDaaXlFfJ2e/lights/1/state' \
      --header 'Content-Type: application/json' \
      --header 'Authorization: Bearer E8rNJawB8g96BqrT5SAgG3tIPOP6' \
-     --data '{"on":false}'
+     --data '{"on":true}'
